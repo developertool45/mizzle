62cf2df9d51e314a8a5a1e10386cdeaf65602328
developertool45 <developertool45@gmail.com>
2024-10-28 12:25:19 +0530

    add javascript

---
+++ index.js
@@ -0,0 +1,7 @@
+var drp3 = document.querySelector(".dropdown-3");
+
+drp3.addEventListener("click", function (e) {
+    e.preventDefault();
+    document.querySelector(".dropdown3-menu").classList.toggle("show");
+    
+});
